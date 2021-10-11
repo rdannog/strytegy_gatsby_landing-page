e11bab3e9364d0477850f17123acebecb845537f
rhayssadandara <rhayssadandara98@gmail.com>
2021-10-11 01:05:09 -0300

    deleting style.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,38 @@ import React from "react"
 import { graphql } from "gatsby"
 import { Header } from "../components/header"
 import { Main } from "../components/main"
-import { GlobalStyle } from "./style"
+import { createGlobalStyle } from "styled-components";
+import Soleto from "../fonts/Soleto-Regular/Soleto-Regular.ttf"
+import Paralucent from "../fonts/Paralucent/Paralucent.ttf"
+
+export const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: "Soleto";
+    src: url(${Soleto});
+  }
+@font-face {
+    font-family: "Paralucent";
+    src: url(${Paralucent});
+}
+body{
+    padding:0;
+    margin: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+    color: #25294C;
+    cursor: default;
+}
+&::-webkit-scrollbar {
+    width: 8px;
+}
+&::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+}
+&::-webkit-scrollbar-thumb {
+    background: rgb(187, 184, 184); 
+}
+`
 
 export const query = graphql`
   query {
